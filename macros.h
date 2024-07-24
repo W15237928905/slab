033dbf0982ed35d6bc3877e9208a4fd3c94ee272
希望队 <t202410256993295@eduxiji.net>
2024-07-24 10:02:09 +0000

    Replace macros.h

--- macros.h
+++ macros.h
@@ -1,10 +1,7 @@
 #pragma once
 /*
-	Returns the pointer to the begining of "type". 
-	Takes a pointer to member of the type
-	Calculates offset of the member from the begining of type
-	Substracts offset from the given pointer
-	Works with linux, should work here too
+	返回指向“type”开头的指针。
+	获取指向该类型成员的指针
 */
 #define list_entry(ptr, type, member) ((type *)((unsigned long long)(ptr)-(unsigned long long)(&((type *)0)->member)))
 #define slab_buffer(slabp) ((unsigned int *)(((slab*)slabp)+1))
