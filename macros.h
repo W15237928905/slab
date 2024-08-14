f3d701ccd51b6e6b7202d0fc12e512169a4561d7
希望队 <t202410256993295@eduxiji.net>
2024-08-14 01:18:23 +0000

    Delete macros.h

--- macros.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-/*
-	返回指向“type”开头的指针。
-	获取指向该类型成员的指针
-*/
-#define list_entry(ptr, type, member) ((type *)((unsigned long long)(ptr)-(unsigned long long)(&((type *)0)->member)))
-#define slab_buffer(slabp) ((unsigned int *)(((slab*)slabp)+1))
-
-
